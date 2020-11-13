f25115920dc7f39189620b8b4fc7132287a6a15a
RuchirDixit <ruchirtd96@gmail.com>
2020-11-13 13:09:30 +0530

    [Ruchir]Added: Test case to check if parking lot is full or not

--- src/test/scala/com/bridgelabz/ParkingLotTest.scala
+++ src/test/scala/com/bridgelabz/ParkingLotTest.scala
@@ -3,13 +3,21 @@ package com.bridgelabz
 import org.scalatest.FunSuite
 
 class ParkingLotTest extends FunSuite {
+  // UC1
   test("givenDetailsDriverShouldBeAbleToParkCar") {
     val status = ParkingLot.addCarToParking("MH12 Al4344","White","0732")
     assert(status == "Parked")
   }
+  // UC2
   test("givenDetailsIfCarexistsDriverShouldBeAbleToUnParkCar") {
     ParkingLot.addCarToParking("MH12 Al4344","White","0732")
     val status = ParkingLot.unParkCar("MH12 Al4344")
     assert(status == "Car Unparked")
   }
+  // UC3
+  test("givenDetailsCheckReturnFalseIfParkingLotNotFull") {
+    ParkingLot.addCarToParking("MH12 Al4344","White","0732")
+    assert(ParkingLot.checkIfLotFull == false)
+  }
+
 }
