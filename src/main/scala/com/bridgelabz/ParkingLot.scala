accb93132dc4d6db963150e355f9838aadabf433
RuchirDixit <ruchirtd96@gmail.com>
2020-11-13 11:50:40 +0530

    [Ruchir]Added: Method to be able to add car to parking lot

--- src/main/scala/com/bridgelabz/ParkingLot.scala
+++ src/main/scala/com/bridgelabz/ParkingLot.scala
@@ -1,8 +1,10 @@
 package com.bridgelabz
+import java.util.Calendar;
 import scala.collection.mutable.Map
+
 class Vehicle(numberPlate : String,colour : String,timeOfPark : String)
 {
-  def getNumberplatevalue = {
+  def getNumberPlateValue = {
     numberPlate
   }
   def getColourValue = {
@@ -15,20 +17,31 @@ class Vehicle(numberPlate : String,colour : String,timeOfPark : String)
 
 object ParkingLot {
   var map : Map[Int,Vehicle] = Map()
-  def addCarToParking = {
-    val numberPlate = scala.io.StdIn.readLine("")
-    map += (1 -> new Vehicle("MH2","White","23:00"))
-    println(map.size)
+
+  def addCarToParking(numberPlate : String,colour : String,timeOfPark : String) = {
+    try
+      {
+      map += (1 -> new Vehicle(numberPlate,colour,timeOfPark))
+      "Parked"
+    }
+    catch {
+      case _:Exception => {
+        "Incorrect Input"
+      }
+    }
   }
   def main(args: Array[String]): Unit = {
     var choice = 99
     while(choice!=0)
       {
-        println("1. Park Car \n 2. Search car \n 3. Unpark car \n 4.Exit (Press 0 )")
+        println("1. Park Car \n 2. Search car \n 3. Unpark car \n 0.Exit")
         val choice = scala.io.StdIn.readInt()
         choice match {
           case 1 => {
-            addCarToParking
+            addCarToParking("MH12","White","1215")
+          }
+          case 2 => {
+
           }
           case 0 => {
             System.exit(1)
