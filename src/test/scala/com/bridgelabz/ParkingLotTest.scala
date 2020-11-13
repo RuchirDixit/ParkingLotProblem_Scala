10471c9b3c8375715e563389bbec0bacac83c39b
RuchirDixit <ruchirtd96@gmail.com>
2020-11-13 12:12:27 +0530

    [Ruchir]Added: Test case to check if driver is able to unpark car

--- src/test/scala/com/bridgelabz/ParkingLotTest.scala
+++ src/test/scala/com/bridgelabz/ParkingLotTest.scala
@@ -7,4 +7,9 @@ class ParkingLotTest extends FunSuite {
     val status = ParkingLot.addCarToParking("MH12 Al4344","White","0732")
     assert(status == "Parked")
   }
+  test("givenDetailsIfCarexistsDriverShouldBeAbleToUnParkCar") {
+    ParkingLot.addCarToParking("MH12 Al4344","White","0732")
+    val status = ParkingLot.unParkCar("MH12 Al4344")
+    assert(status == "Car Unparked")
+  }
 }
